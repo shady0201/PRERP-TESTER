36c0ff905a985575ab6734c17d97421030e67301
shady0201 <64774943+shady0201@users.noreply.github.com>
2026-01-28 14:22:44 +0700

    update

--- PRERP-TESTER/Extensions/WebView2Extensions.cs
+++ PRERP-TESTER/Extensions/WebView2Extensions.cs
@@ -1,40 +1,56 @@
-﻿using PRERP_TESTER.ViewModels;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using PRERP_TESTER.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.Concurrent;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace PRERP_TESTER.Extensions
 {
-    public class WebView2Extensions
+    public static class WebView2Extensions
     {
+        private static readonly ConcurrentDictionary<string, Task<CoreWebView2Environment>> _envCache = new();
+
         public static readonly DependencyProperty AccountIDProperty =
-        DependencyProperty.RegisterAttached("AccountID", typeof(string), typeof(WebView2Extensions),
-            new PropertyMetadata(null, OnAccountIDChanged));
+            DependencyProperty.RegisterAttached("AccountID", typeof(string), typeof(WebView2Extensions),
+                new PropertyMetadata(null, OnAccountIDChanged));
 
         public static string GetAccountID(DependencyObject obj) => (string)obj.GetValue(AccountIDProperty);
         public static void SetAccountID(DependencyObject obj, string value) => obj.SetValue(AccountIDProperty, value);
 
         private static async void OnAccountIDChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is Microsoft.Web.WebView2.Wpf.WebView2 webView && e.NewValue is string accountId)
+            if (d is WebView2 webView && e.NewValue is string accountId && !string.IsNullOrEmpty(accountId))
             {
-
-                string userDataFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sessions", accountId);
-
- 
-                var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync(userDataFolder: userDataFolder);
-
-                await webView.EnsureCoreWebView2Async(env);
-
-                // Sau khi khởi tạo xong mới nạp URL (để đảm bảo ăn session)
-                if (webView.DataContext is TabViewModel vm)
+                try
                 {
-                    webView.Source = new Uri(vm.Url);
+                    var envTask = _envCache.GetOrAdd(accountId, id => CreateEnvironmentForAccount(id));
+
+                    var env = await envTask;
+                    await webView.EnsureCoreWebView2Async(env);
+                    var context = webView.DataContext;
+
+                    if (webView.DataContext is TabViewModel vm && vm.Url != null)
+                    {
+                        webView.Source = new Uri(vm.Url.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine($"WebView2 Init Error: {ex.Message}");
                 }
             }
+        }
+        private static Task<CoreWebView2Environment> CreateEnvironmentForAccount(string accountId)
+        {
+            string userDataFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sessions", $"Account_{accountId}");
+
+            if (!Directory.Exists(userDataFolder))
+                Directory.CreateDirectory(userDataFolder);
+
+            return CoreWebView2Environment.CreateAsync(userDataFolder: userDataFolder);
         }
     }
 }
